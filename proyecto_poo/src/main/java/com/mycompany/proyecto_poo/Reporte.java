9b0c6ac0492f229f2c7792ca6d0139567c6a0d01
JesusVsRcha <104877392+JesusVsRcha@users.noreply.github.com>
2023-04-22 22:55:15 -0400

    Jesus
    Reporte class leer un excel

--- proyecto_poo/src/main/java/com/mycompany/proyecto_poo/Reporte.java
+++ proyecto_poo/src/main/java/com/mycompany/proyecto_poo/Reporte.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.proyecto_poo;
+import java.io.File;
+import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.*;
 
-/**
- *
- * @author diego
- */
 public class Reporte {
-    
+    public static void main(String[] args) {
+        try {
+            // Carga el archivo de Excel
+            File archivo = new File("datos.xlsx");
+            FileInputStream inputStream = new FileInputStream(archivo);
+            
+            // Crea una instancia de la clase Workbook para leer el archivo
+            Workbook libro = WorkbookFactory.create(inputStream);
+            
+            // Obtiene la hoja del libro que quieres leer (en este caso, la primera)
+            Sheet hoja = libro.getSheetAt(0);
+            
+            // Recorre las filas de la hoja
+            for (Row fila : hoja) {
+                // Recorre las celdas de cada fila
+                for (Cell celda : fila) {
+                    // Obtiene el valor de la celda y lo muestra en la consola
+                    String valor = celda.getStringCellValue();
+                    System.out.print(valor + "\t");
+                }
+                System.out.println();
+            }
+            
+            // Cierra el archivo
+            libro.close();
+            inputStream.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
