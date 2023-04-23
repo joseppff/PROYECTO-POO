9782fd996897d865443f605f5e9a37c86d58db97
OWO122 <73804717+OWO122@users.noreply.github.com>
2023-04-22 22:12:51 -0400

    setters y getters(conductor

--- proyecto_poo/src/main/java/com/mycompany/proyecto_poo/Conductor.java
+++ proyecto_poo/src/main/java/com/mycompany/proyecto_poo/Conductor.java
@@ -10,4 +10,34 @@ package com.mycompany.proyecto_poo;
  */
 public class Conductor {
     
+    private String Nombre;
+    private String Apellido;
+    private int RUT;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public int getRUT() {
+        return RUT;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public void setRUT(int RUT) {
+        this.RUT = RUT;
+    }
+    
+    
+    
 }
