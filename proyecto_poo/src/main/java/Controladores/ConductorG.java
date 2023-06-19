4544904c97ee6c59a64cc29b0ced7a92dc9c0beb
OWO122 <73804717+OWO122@users.noreply.github.com>
2023-06-18 22:14:40 -0400

    Se agregan clases para bd conexion

--- proyecto_poo/src/main/java/Controladores/ConductorG.java
+++ proyecto_poo/src/main/java/Controladores/ConductorG.java
@@ -3,11 +3,88 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controladores;
+import com.mycompany.proyecto_poo.*;
+import com.mycompany.proyecto_poo.InicioSesion.*;
+/**
+ *
+ * @author diego
+ */
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import Modelos.ClienteDB;
+import Modelos.Cliente;
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author diego
  */
-public class ConductorG {
+public class ConductorG implements Crud {
+    public String query;
+    
+    public ArrayList<Conductor> Leer(Connection link){
+        
+        Conductor conductor=new Conductor();
+        try{
+            Statement s = link.createStatement();
+            query="select * from Clientes";
+            ResultSet rs=s.executeQuery(query);
+            while (rs.next()){
+                
+               cliente.setRut(rs.getString("rut"));
+               cliente.setNombres(rs.getString("nombres"));
+               cliente.setApellidos(rs.getString("apellidos"));
+               cliente.setFechaNacimiento(rs.getString("fechaNacimiento"));
+               cliente.setRegion(rs.getString("region"));
+               cliente.setComuna(rs.getString("comuna"));
+               
+               ListaCliente.add(cliente);
+                
+            }
+            
+            return ListaCliente;
+        }catch (SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public void Actualizar(){
+        
+    }
+    
+    public void Eliminar(){
+        
+    }
+    
+    public boolean Crear(Connection link, Conductor conductor){
+        
+        try{
+            Statement s = link.createStatement();
+            query="insert into Conductor(nombres,apellido,rut)values('"+conductor.getNombre()+"','"+conductor.getApellido()+"','"+conductor.getRut()+"')";
+            s.executeUpdate(query);
+            return true;
+            
+        }catch (SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+        
+    }
     
 }
